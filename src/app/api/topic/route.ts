ee90ddf32c042390c13428ceade366368e04bddd
vinicius <vinicandido00@gmail.com>
2024-04-19 20:55:30 -0300

    working on crud feature

---
+++ src/app/api/topic/route.ts
@@ -0,0 +1,30 @@
+import prismaConnection from "@/libs/mysql";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request:any){
+    const {title,description} = await request.json();
+
+    const topic = await prismaConnection.topic.create({data:{title,description}});
+    console.log(topic);
+    return NextResponse.json({message:"topic created successfully"}, {status: 201});
+}
+
+export async function GET(){
+    const topics = await prismaConnection.topic.findMany();
+    return NextResponse.json({ topics});
+}
+
+export async function DELETE(request: NextRequest){
+    const idString = request.nextUrl.searchParams.get("id");
+    let id: number | undefined;
+    
+    if(idString){
+        id = parseInt(idString, 10);
+    }
+    
+    if(id != undefined){
+        await prismaConnection.topic.delete({where: {id}});
+    }
+
+    return NextResponse.json({message:"topic deleted successfully"}, {status: 200});
+}
---
+++ src/components/EditTopicForm.tsx
@@ -0,0 +1,17 @@
+export default function EditTopicForm(){
+    return <form className="flex flex-col gap-3 items-center">
+        <input 
+            className="border border-slate-500 px-8 py-2 w-full"
+            type="text"
+            placeholder="Topic Title" />
+
+        <input 
+            className="border border-slate-500 px-8 py-2 w-full"
+            type="text"
+            placeholder="Topic Description" />
+
+        <button className="bg-green-600
+        text-white py-3 px-6 w-fit">Update Topic</button>
+        
+    </form>
+}
